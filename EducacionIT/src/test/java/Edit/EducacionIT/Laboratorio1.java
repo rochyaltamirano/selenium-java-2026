13cbf7441e311e7aa0a2407b811943f9ab9ecbe6
Rocio Altamirano <rochyaltamirano@gmail.com>
2026-04-14 15:15:11 -0300

    primer commit

---
+++ EducacionIT/src/test/java/Edit/EducacionIT/Laboratorio1.java
@@ -0,0 +1,20 @@
+package Edit.EducacionIT;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Laboratorio1 {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+    	System.out.println("Mi primer test");
+        assertTrue(true);
+    }
+}
